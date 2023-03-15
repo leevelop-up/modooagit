a73f7b95e747201bd0cec62a5c6c5ade5c0bcd9c
leevelop <merong0075@gmail.com>
2023-03-15 20:56:48 +0900

    인텔리제이 테스트

--- src/main/java/com/example/modooagit/ModooagitApplication.java
+++ src/main/java/com/example/modooagit/ModooagitApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModooagitApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ModooagitApplication.class, args);
 	}
