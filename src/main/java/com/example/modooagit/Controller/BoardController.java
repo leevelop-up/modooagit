5bcd2aa1d1cc241a5d3bc5b9087ec38281d2bdf1
leevelop <merong0075@gmail.com>
2023-03-17 00:15:10 +0900

    인텔리제이 테스트

--- src/main/java/com/example/modooagit/Controller/BoardController.java
+++ src/main/java/com/example/modooagit/Controller/BoardController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // 어노테이션을 입력하면 알아서 상단에 import 가 된다!
-
-@RequestMapping("/board/")
 public class BoardController {
-
-    @GetMapping("/hello")
-    public String Hello() {
-        return "/boards/hello";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
+
+
