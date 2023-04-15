0460cb0f747c0a07c1257c201d83da18f590baad
leevelop <merong0075@gmail.com>
2023-04-16 01:03:23 +0900

    하루 30분

--- src/main/java/com/example/modooagit/Controller/BoardController.java
+++ src/main/java/com/example/modooagit/Controller/BoardController.java
@@ -3,12 +3,15 @@ package com.example.modooagit.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller // 어노테이션을 입력하면 알아서 상단에 import 가 된다!
+//@Controller // 어노테이션을 입력하면 알아서 상단에 import 가 된다!
+@RestController
 public class BoardController {
-    @GetMapping("/")
+    //@RequestMapping("/test")
+    @GetMapping(value="/api/helloWorld")
     public String index() {
-        return "index";
+        return "Greetings from Spring Boot!";
     }
 }
 
