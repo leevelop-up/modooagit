00a6b0ead1008494a2c804b09f2e299f699214a7
leevelop <merong0075@gmail.com>
2023-05-15 23:43:58 +0900

    강의 따라하기 TDD

--- src/main/java/com/example/modooagit/domain/Restaurant.java
+++
@@ -1,4 +0,0 @@
-package com.example.modooagit.domain;
-
-public class Restaurant {
-}
--- src/main/java/com/example/modooagit/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.modooagit.repository;
-
-import com.example.modooagit.domain.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
