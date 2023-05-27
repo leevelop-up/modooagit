b3a43673e20eaa752841c52c5ef3bb9c77fd58df
leevelop <merong0075@gmail.com>
2023-05-27 23:00:16 +0900

    의존성 주입 DI

--- src/main/java/com/example/modooagit/service/MemberService.java
+++ src/main/java/com/example/modooagit/service/MemberService.java
@@ -3,14 +3,21 @@ package com.example.modooagit.service;
 import com.example.modooagit.domain.Member;
 import com.example.modooagit.repository.MemberRepository;
 import com.example.modooagit.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    
-    
+
+    private final MemberRepository memberRepository;
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
     public Long join(Member member){
 
         validateDuplicateMember(member);
@@ -20,9 +27,9 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
-            .ifPresent(m -> {
-            throw new IllegalStateException("이미 값이 있습니다");
-        });
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 값이 있습니다");
+                });
     }
     public List<Member> findMembers(){
         return memberRepository.findAll();
